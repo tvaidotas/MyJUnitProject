ef267e57c48def0dcbcb0836c9a569f6e0e991f2
tvaidotas <tadas.vaidotas@gmail.com>
2019-08-20 13:11:15 +0100

    initial commit

---
+++ src/main/java/com/qa/App.java
@@ -0,0 +1,14 @@
+package com.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(args[0]);
+        System.out.println( "Hello World!" );
+    }
+}
